fc099e072b94df1b3bed74fa3fdff69d7bc3410e
Janindu Malshan <75658442+ImJanindu@users.noreply.github.com>
2021-04-07 19:38:41 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+# @ImJanindu
+# @Infinity_Bots
+
+import os
+
+BOTTOKEN = "" # your bot token frpm @BotFather
+APIID =      # your api id from https://my.telegram.org/
+APIHASH = "" # your api hash from https://my.telegram.org/
+DOWNLOADPATH = "Downloads/" # don't change
+USERNAME = "" # put your bot username here
