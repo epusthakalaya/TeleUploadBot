49db3b16641d4b1598fc362d0d144d08dd54c8fe
Janindu Malshan <75658442+ImJanindu@users.noreply.github.com>
2021-04-07 20:49:53 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 # Infinity Bots <https://t.me/Infinity_Bots>
 # @ImJanindu
 
-from telethon import TelegramClient, events
+from telethon import TelegramClient, events, Button
 from urldownload import download_file
 from config import BOTTOKEN, APIID, APIHASH, DOWNLOADPATH, USERNAME
 import os
@@ -53,7 +53,10 @@ async def send_to_tmp_async(file):
 @bot.on(events.NewMessage(pattern='/start'))
 async def start(event):
     #send a message when the command /start is issued.
-    await event.respond('Hello, I am Upload Bot.\n\nI can upload direct links to Telegram and generate download links for files uploaded in telegram.\n\nSend me any direct link and reply it with /upload for upload it to Telegram as file.\n\nSend me any file and reply it with /transfersh or /tmpninja to generate direct download link of that file.\n\nMade with ❤ 🇱🇰 by @Infinity_BOTs.')
+    await event.respond('Hello, I am Upload Bot.\n\nI can upload direct links to Telegram and generate download links for files uploaded in telegram.\n\nSend me any direct link and reply it with /upload for upload it to Telegram as file.\n\nSend me any file and reply it with /transfersh or /tmpninja to generate direct download link of that file.\n\nA bot by @Infinity_BOTs.',
+                         buttons=[
+                        [Button.url("Source Code", url="https://github.com/ImJanindu/TeleUploadBot"),
+                         Button.url("Dev", url="https://t.me/Infinity_BOTs")]])
     raise events.StopPropagation
 
 @bot.on(events.NewMessage(pattern='/upload'))
